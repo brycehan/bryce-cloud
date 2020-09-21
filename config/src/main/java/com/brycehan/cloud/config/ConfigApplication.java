9920288efc4c02edb4e4ab7c7cff0d89741999c5
韩世强 <brycehan@163.com>
2020-09-21 15:27:20 +0800

    调整配置

--- config/src/main/java/com/brycehan/cloud/config/ConfigApplication.java
+++ config/src/main/java/com/brycehan/cloud/config/ConfigApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableConfigServer
 @EnableDiscoveryClient
@@ -14,4 +18,15 @@ public class ConfigApplication {
 		SpringApplication.run(ConfigApplication.class, args);
 	}
 
+	@Configuration
+	@EnableWebSecurity
+	static class WebSecurityConfiguration extends WebSecurityConfigurerAdapter{
+		
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().disable();
+			super.configure(http);
+		}
+
+	}
 }
