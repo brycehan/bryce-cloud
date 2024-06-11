98a0b717b9e9763efec30698602fc1bc8c8d235e
Bryce Han <brycehan@163.com>
2024-06-11 20:21:48 +0800

    优化import

--- bryce-cloud-common/bryce-common-security/src/main/java/com/brycehan/cloud/common/security/common/interceptor/SecurityContextInterceptor.java
+++ bryce-cloud-common/bryce-common-security/src/main/java/com/brycehan/cloud/common/security/common/interceptor/SecurityContextInterceptor.java
@@ -2,8 +2,8 @@ package com.brycehan.cloud.common.security.common.interceptor;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.extern.slf4j.Slf4j;
 import jakarta.validation.constraints.NotNull;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
