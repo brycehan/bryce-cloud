0d619b7dd931fc23b29c1b67450a47ff2e281e22
Bryce Han <brycehan@163.com>
2025-01-03 04:35:38 +0800

    refactor(bryce-common-core): 优化 ExcelUtils 文件名编码逻辑
    - 使用 String.concat() 替代 + 操作符拼接字符串，提高性能
    - 优化了文件名编码逻辑，防止中文乱码问题

--- bryce-cloud-common/bryce-common-core/src/main/java/com/brycehan/cloud/common/core/util/ExcelUtils.java
+++ bryce-cloud-common/bryce-common-core/src/main/java/com/brycehan/cloud/common/core/util/ExcelUtils.java
@@ -47,7 +47,7 @@ public class ExcelUtils {
             response.setCharacterEncoding("utf-8");
             // 这里URLEncoder.encode可以防止中文乱码 当然和easyexcel没有关系
             String filenameEncoded = URLEncoder.encode(filename, StandardCharsets.UTF_8).replaceAll("\\+", "%20");
-            response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename*=utf-8''" + filenameEncoded + ".xlsx");
+            response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename*=utf-8''".concat(filenameEncoded).concat(".xlsx"));
             response.setHeader(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS, HttpHeaders.CONTENT_DISPOSITION);
             // 这里需要设置不关闭流
             EasyExcel.write(response.getOutputStream(), head)
